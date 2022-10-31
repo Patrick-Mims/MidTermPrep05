c83207c898698d1c848111d5f8fa49190f430589
Patrick Mims <patrickkevinmims@gmail.com>
2022-10-31 15:49:00 -0700

    Initial Commit

---
+++ MidTermPrep05/MidTermPrep05App.swift
@@ -0,0 +1,17 @@
+//
+//  MidTermPrep05App.swift
+//  MidTermPrep05
+//
+//  Created by JPL-ST-SPRING2022 on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct MidTermPrep05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
